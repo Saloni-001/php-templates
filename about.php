f084d5f413692fdb72f1797e8da1b23d37e332b1
Saloni-001 <84764292+Saloni-001@users.noreply.github.com>
2021-06-08 19:10:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>janki panchal</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './themepart/menu.php';
+include './themepart/logo.php';
+?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="tree-276014__340.jpg" width="920" height="400" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">About </a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    hi i am janki panchal from akash infotech visit my website.
+                                    
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<?php
+                include './themepart/sidebar.php';
+                ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+                include './themepart/footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
+
+
